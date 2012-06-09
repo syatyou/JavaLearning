b20dd336e5ea6861c6a16f24eb14b10b3232981e
itacademy <itacademy@192.168.1.11>
2012-06-09 20:40:51 +0900

    びっくりマークを追加

--- src/jp/itacademy/samples/HelloWorld.java
+++ src/jp/itacademy/samples/HelloWorld.java
@@ -3,7 +3,7 @@ package jp.itacademy.samples;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		System.out.println("HelloWorld!!");
 	}
 
 }
